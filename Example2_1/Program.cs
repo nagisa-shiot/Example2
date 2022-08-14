0550104a618a8cb5efd07157543665a1d9fe777a
Shchiptsov Danil <bondar-seryy@mail.ru>
2022-08-14 20:02:11 +0800

    Task 10

---
+++ Example2_1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифа этого числа -> "+stringNumber[1]);
